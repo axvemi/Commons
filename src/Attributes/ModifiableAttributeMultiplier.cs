5a63c9d42f439a7ab275832effbeb4ec13f633ad
axvemi <axvemi@gmail.com>
2024-06-21 10:04:09 +0200

    Removed unneeded inheritance

--- src/Attributes/ModifiableAttributeMultiplier.cs
+++ src/Attributes/ModifiableAttributeMultiplier.cs
@@ -1,14 +1,11 @@
 
 namespace Axvemi.Commons.Attributes;
 
-public class ModifiableAttributeMultiplier : ModifiableAttributeNumber<float>
+public class ModifiableAttributeMultiplier : ModifiableAttribute<float>
 {
     public ModifiableAttributeMultiplier(float baseValue) : base(baseValue)
     {
     }
 
-    protected override float Add(float value1, float value2)
-    {
-        return value1 * value2;
-    }
+    protected override float Add(float value1, float value2) => value1 * value2;
 }
